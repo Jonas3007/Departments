1a0dd6ba8bbecc2e0f7041dedabf98180988d734
Jonas3007 <kormannjonas@gmail.com>
2026-03-04 07:20:35 +0100

    again fcking build folder

--- 01_SdCustomization/01_QTCalculator/build/CMakeFiles/qtcalc.dir/qtcalc_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/qtcalc.dir/qtcalc_autogen/mocs_compilation.cpp.o: \
- /home/korman4/development/Departments/01_SdCustomization/01_QTCalculator/build/qtcalc_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
